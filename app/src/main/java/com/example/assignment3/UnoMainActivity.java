d763c9d8218f67ff35f586b49a350232fcfdc3a0
starrnakamitsu <nakamits26@up.edu>
2023-03-15 23:43:21 -0700

    Got to the third section in the final checkpoint

--- app/src/main/java/com/example/assignment3/UnoMainActivity.java
+++ app/src/main/java/com/example/assignment3/UnoMainActivity.java
@@ -1,7 +1,11 @@
 package com.example.assignment3;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,6 +27,11 @@ public class UnoMainActivity extends GameMainActivity {
 
         // Initialize the layout
         setContentView(R.layout.activity_main);
+
+        // Sets the button's onClickListener to the MainActivity
+        Button run = findViewById(R.id.run);
+        run.setOnClickListener(this);
+
     }
 
     @Override
@@ -76,11 +85,11 @@ public class UnoMainActivity extends GameMainActivity {
     //secondInstance.
     //– Using firstInstance, call each method in the game state class at least
     //once. In each case it should be making a legal move in the game.
-    //For each method call, a brief description of the action taken should
-    //be printed to the multi-line EditText. (e.g., “Player 1 has moved his
-    //pawn from position 10 to position 14.” or “Player 3 has rolled the
-    //dice. She rolled a 9.” New messages should be appended to previous
-    //ones, not overwrite them.
+    //For each  the multi-line EditText. (e.g., “Player 1 has moved his
+    //    //pawn from position 10 to position 14.” or “Player 3 has rolled the
+    //    //dice. She rolled a 9.” New messages should be appended to previous
+    //    //ones, not overwrite them.method call, a brief description of the action taken should
+    //be printed to
     //2
     //– Create a new instance of the game state class using the default constructor. Assign this value to a variable named thirdInstance.
     //– Use your deep copy constructor to make a deep copyof thirdInstance
@@ -90,5 +99,30 @@ public class UnoMainActivity extends GameMainActivity {
     //The two strings should be identical. Your code should verify this.
     //Also, print both strings to the multi-line EditText for visual inspection. Again, append these rather than overwrite previous messages.
 
+
+    // Everything that will occur when the button is clicked
+    public void onClick(View v){
+        // This deletes any existing text
+        EditText text = findViewById(R.id.editText1);
+        text.setText("");
+
+        // new instance of the UnoGameState class
+        UnoGameState firstInstance = new UnoGameState(7, 7, 2, 1);
+
+        // deep copy of firstInstance
+        UnoGameState secondInstance = new UnoGameState(firstInstance);
+
+        // calling all of the methods in UnoGameState using firstInstance
+        // dummy parameters are used when needed
+        text.append("The Player's hand size is " + firstInstance.getHandSize() + ".\n");
+        text.append("There are " + firstInstance.getPlayerNum() + " players in the game.\n");
+        text.append("The current number in play is " + firstInstance.getNumInPlay() + ".\n");
+        text.append("The current color in play is " + firstInstance.getColorInPlay() + ".\n");
+        firstInstance.setNumInPlay(1, 1);
+        firstInstance.setCurrentTurn();
+        firstInstance.incremHandSize(3, true);
+
+
+    }
 
 }
