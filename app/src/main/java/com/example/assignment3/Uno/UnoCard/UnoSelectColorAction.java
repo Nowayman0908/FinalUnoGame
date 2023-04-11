fc989ab5546b9c0e11bb0af92103d9185d347113
starrnakamitsu <nakamits26@up.edu>
2023-04-11 16:09:07 -0700

    completed skip, draw 2, and reverse methods updated UnoSelectColorAction

--- app/src/main/java/com/example/assignment3/Uno/UnoCard/UnoSelectColorAction.java
+++ app/src/main/java/com/example/assignment3/Uno/UnoCard/UnoSelectColorAction.java
@@ -4,12 +4,15 @@ import GameFramework.actionMessage.GameAction;
 import GameFramework.players.GamePlayer;
 
 public class UnoSelectColorAction extends GameAction {
+    public int selectedColor; // depending on the color the player chooses, this number will change (0-3)
+
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public UnoSelectColorAction(GamePlayer player) {
+    public UnoSelectColorAction(GamePlayer player, int selectedColor) {
         super(player);
+        this.selectedColor = selectedColor;
     }
 }
